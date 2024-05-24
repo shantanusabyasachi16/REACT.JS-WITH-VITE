b52871cc58676b2cbebafcb3c4dfc16425a104e3
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-05-24 17:16:27 +0530

    modified 1 and added 2

--- 02vitereact/src/App.jsx
+++ 02vitereact/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [number, setNumber] = useState(0)
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,8 +18,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setNumber((number) => number + 1)}>
-        number is {number}
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
