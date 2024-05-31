c84d741810f18ac087cb86615817183f73a56d38
Shantanu Swain <137885216+shantanusabyasachi16@users.noreply.github.com>
2024-05-31 12:19:29 +0530

    using useRef hooks with useCallback and accessing the windows clipboard

--- 08passwordGenerator/src/App.jsx
+++ 08passwordGenerator/src/App.jsx
@@ -59,7 +59,7 @@ function App() {
 
   return (
     <>
-        <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-900 text-orange-500">
           <h1 className='text-white text-center my-3' >password generator</h1>
           <div className="flex shadow rounded-lg overflow-hidden mb-4">
             <input 
